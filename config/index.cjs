6d163586fffee295ec38d7651b5da04a3a2704d6
voyager03 <129132771+voyager03@users.noreply.github.com>
2023-03-28 16:54:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -28,6 +28,15 @@ const USER_CONFIG = {
       id: 'oKECb5iJ3xV52VfX3Yb4fUVkOGsw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'qltmDpUlnTYlcTKRHaQ-bFQNrDRCxdXqVNDvJBEJIsE',
+     
+     
+     // 想要发送的人的名字
+      name: '宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oKECb5umTXIxbUzvgvHTxlqJ6Y_g',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'qltmDpUlnTYlcTKRHaQ-bFQNrDRCxdXqVNDvJBEJIsE',
+     
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
