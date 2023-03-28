f5fa08a1c1722c70255b6926c6a347b7f4e21641
voyager03 <129132771+voyager03@users.noreply.github.com>
2023-03-28 10:08:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,56 +8,56 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx8424afd6b1253806',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '353d3bfb4ea0bd6a1f1b0ca38313c14d',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '北京市',
+  CITY: '海淀区',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oKECb5iJ3xV52VfX3Yb4fUVkOGsw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'Y5jz89a7fqhbi-DEPIT-hUFEP9wtRDLjb0PIsHr93OE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      //自加horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+       //自加 {
+          type: '*生日', name: '宝贝', year: '2000', date: '01-08',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+        //自加 {
+          type: '生日', name: '宝贝', year: '2000', date: '02-12',
         },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+        //自加{
+          type: '节日', name: '相识纪念日', year: '2023', date: '02-00',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-02-25' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+       //自加 { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'PxHCO0X7ElEuBrDggF9g71e_Yx7TzmJl96mmbCXV_Oc',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oKECb5iJ3xV52VfX3Yb4fUVkOGsw',
     }
   ],
 
