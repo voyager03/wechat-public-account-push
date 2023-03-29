2247cca418aabdc18035a43f23abeb243f4dd1cf
voyager03 <129132771+voyager03@users.noreply.github.com>
2023-03-29 15:58:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,14 +20,15 @@ const USER_CONFIG = {
   PROVINCE: '北京市',
   CITY: '海淀区',
 
-  USERS: [
-    {
+  USERS: 
+ [
+ {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '宝贝','宝贝2'
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU21348T8Ektv33L5beRmOkZIsgctRyJqSwg0jeM',
+      id: 'PDU21348T8Ektv33L5beRmOkZIsgctRyJqSwg0jeM','PDU21348TvJbkwoGvA3s7JAFLlQqp3F2IkUgdjMLf'
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: '0001','0002'
      
      
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -47,59 +48,16 @@ const USER_CONFIG = {
      
      ],
       // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
+      customizedDateList: 
+     [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-02-25' },
         // 结婚纪念日
-       { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  
-  
-  
-     
-     
-     
-     
-     
-     
-     
-     
-     
-       {
-      // 想要发送的人的名字
-      name: '宝贝1',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU21348TvJbkwoGvA3s7JAFLlQqp3F2IkUgdjMLf',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
-     
-     
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-     {
-          type: '*生日', name: '宝贝', year: '2000', date: '01-08',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '宝贝', year: '2000', date: '02-12',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2023', date: '02-00',
-        },
-     
+         { keyword: 'marry_day', date: '2022-09-09' },
      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2023-02-25' },
-        // 结婚纪念日
-       { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },  
-     
+ },
+  
+ 
      
    ],
 
@@ -113,7 +71,7 @@ const USER_CONFIG = {
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU21348T8Ektv33L5beRmOkZIsgctRyJqSwg0jeM',
     }
-  ],
+],
 
 }
 
