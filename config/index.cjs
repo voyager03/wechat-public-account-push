0f698ac0f2d7cb236fff062957e67d4838fa3a4e
voyager03 <129132771+voyager03@users.noreply.github.com>
2023-03-29 08:07:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -27,7 +27,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oKECb5iJ3xV52VfX3Yb4fUVkOGsw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'qltmDpUlnTYlcTKRHaQ-bFQNrDRCxdXqVNDvJBEJIsE',
+      useTemplateId: 'JHSZI_YNqhF4_WJlsXpPAB4HDqqC6Q6Qz_GmaSq3Bak',
      
      
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
